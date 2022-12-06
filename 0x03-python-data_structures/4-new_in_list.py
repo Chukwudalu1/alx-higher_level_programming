45a4a6863d7c23d7d5fde96ca9de1cf01c56174c
Chukwudalu1 <chukwudalueze01@gmail.com>
2022-12-06 06:17:20 -0800

    python tasks

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or > len(my_list):
+        return (my_list)
+    else:
+        copy_list = my_list.copy
+        return (new_list)
